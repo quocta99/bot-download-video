b2aaab593410da736d2f8d5f8414576409a8ce73
Quốc Bình <quocbinh@Quocs-MacBook-Pro.local>
2022-02-12 23:06:31 +0700

    -

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use((req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
 const { getLinkVideoFacebook, getLinkVideoYoutube } = require('./service')
 
 app.get('/', (req, res) => {
